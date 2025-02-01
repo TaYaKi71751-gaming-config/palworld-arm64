4c6e79294de5b25f0e9723c27465558d8d032c1e
TaYaKi71751 <shantian.tw@gmail.com>
2025-02-01 21:08:55 +0900

    Add run.sh,docker-compose.yml

---
+++ run.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+ORIGIN_PWD="$PWD"
+while true
+do
+	sudo docker rm -f palworld-server
+	mkdir -p palworld
+	sudo chown -R $USER:$USER palworld
+	if ( ls "${ORIGIN_PWD}/palworld/Pal/Saved/SaveGames" );then
+		cd "${ORIGIN_PWD}/palworld/Pal/Saved/SaveGames/"
+		MAP_PATHS="$(find . -mindepth 2 -maxdepth 2)"
+		while IFS= read -r MAP_PATH
+		do
+			echo ${MAP_PATH}
+			cd "${ORIGIN_PWD}/palworld/Pal/Saved/SaveGames${MAP_PATH}"
+			git add -A
+			git commit -m "$(date +%s) ${MAP_PATH}"
+			git push
+		done < <(printf '%s\n' "${MAP_PATHS}")
+		cd "${ORIGIN_PWD}"
+		rm -rf "${ORIGIN_PWD}/palworld/Pal/Binaries"
+	fi
+	sudo chown 1000:1000 -R "${ORIGIN_PWD}/palworld"
+	sudo docker-compose up -d
+	sleep 3600
+done
